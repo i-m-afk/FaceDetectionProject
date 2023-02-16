b069e8a0bc3007237a5a21253c9197dc527a2647
Rishav Kumar <85122792+i-m-afk@users.noreply.github.com>
2023-02-16 17:02:39 +0530

    Encapsulation and error handling

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
 import mediapipe as mp
 import time
-import  cv2
+import cv2
+
 cap = cv2.VideoCapture(0)
 
 pTime = 0
@@ -14,27 +15,28 @@ while True:
 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = faceDetection.process(imgRGB)
-    print(results)
 
     if results.detections:
         for id, detection in enumerate(results.detections):
-            # mpDraw.draw_detection(img, detection)
-            # print(id, detection)
-            # print(detection.score)
-            # print(detection.location_data.relative_bounding_box)
             bboxC = detection.location_data.relative_bounding_box
             ih, iw, ic = img.shape
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
-                    int(bboxC.width * iw), int(bboxC.height * ih)
+                int(bboxC.width * iw), int(bboxC.height * ih)
             cv2.rectangle(img, bbox, (255, 0, 0), 2)
             cv2.putText(img, f'{int(detection.score[0] * 100)}%',
                         (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,
                         3, (0, 255, 0), 2)
 
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
-                    3, (0, 255, 0), 2)
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (0, 255, 0), 2)
+
+    cv2.imshow("Image", img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
